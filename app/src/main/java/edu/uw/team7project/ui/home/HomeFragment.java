23cf7ffba9611c2781f7fbb80feb2310849b8fd2
MultipleCrane9 <bjjlaird@hotmail.com>
2020-05-04 11:17:36 -0700

    Mostly done, just need to merge and test.

--- app/src/main/java/edu/uw/team7project/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/team7project/ui/home/HomeFragment.java
@@ -2,13 +2,18 @@ package edu.uw.team7project.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.team7project.R;
+import edu.uw.team7project.databinding.FragmentHomeBinding;
+import edu.uw.team7project.model.UserInfoViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,5 +30,20 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Local access to the ViewBinding object. No need to create as Instance Var as it is only
+        //used here.
+        FragmentHomeBinding binding = FragmentHomeBinding.bind(getView());
+
+
+
+
+        //  binding.textEmail.setText("Welcome Home!");
+
+
     }
 }
--- app/src/main/java/edu/uw/team7project/ui/messages/MessagesFragment.java
+++ app/src/main/java/edu/uw/team7project/ui/messages/MessagesFragment.java
@@ -2,6 +2,8 @@ package edu.uw.team7project.ui.messages;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.team7project.R;
+import edu.uw.team7project.databinding.FragmentHomeBinding;
+import edu.uw.team7project.databinding.FragmentMessagesBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,5 +29,21 @@ public class MessagesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Local access to the ViewBinding object. No need to create as Instance Var as it is only
+        //used here.
+        FragmentMessagesBinding binding = FragmentMessagesBinding.bind(getView());
+
+
+
+
+        //  binding.textEmail.setText("Welcome Home!");
+
+
     }
 }
