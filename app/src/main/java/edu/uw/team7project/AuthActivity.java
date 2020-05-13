8ce10620028e3b13911962a9bd6d37e3b49c91b2
Yousif Azami <yousifazami@outlook.com>
2020-05-12 18:50:39 -0700

    created night mode theme in login and register fragments

--- app/src/main/java/edu/uw/team7project/AuthActivity.java
+++ app/src/main/java/edu/uw/team7project/AuthActivity.java
@@ -1,19 +1,59 @@
 package edu.uw.team7project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
 
 /**
  * An activity representing the authentication process for the application.
  *
+ * @author Yousif Azami
  * @author Trevor Nichols
  */
 public class AuthActivity extends AppCompatActivity {
 
+    private Switch myswitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_auth);
+
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        }
+        else setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        myswitch = (Switch)findViewById(R.id.mySwitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    recreate();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    recreate();
+                }
+            }
+        });
+
+
     }
+//
+//    public void restartApp() {
+//        Intent i = new Intent(getApplicationContext(), AuthActivity.class);
+//        startActivity(i);
+//        finish();
+//    }
 }
